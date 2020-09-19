27d0fa085790ff756bb17a1c925b1ebc82911a23
Morgan Farrow <morgancfarrow@gmail.com>
2020-09-19 13:03:14 +0000

    Automatically backed up by Learn

--- weather_watcher_two/lib/weather_watcher_two.rb
+++
@@ -1,6 +0,0 @@
-require "weather_watcher_two/version"
-
-module WeatherWatcherTwo
-  class Error < StandardError; end
-  # Your code goes here...
-end
--- weather_watcher_two/lib/weather_watcher_two/version.rb
+++
@@ -1,3 +0,0 @@
-module WeatherWatcherTwo
-  VERSION = "0.1.0"
-end
